9799cf7c68e6a83a0dcf42561d4a3ec7038ecfed
MaryamJokar <maryam.79jokar@gmail.com>
2021-11-12 08:48:11 +0330

    while loop

--- example-project/R P S.py
+++ example-project/R P S.py
@@ -15,3 +15,12 @@ elif randomNumber == 1:
 elif randomNumber == 2:
     computerMove = "scissors"
 
+player1_wins = 0
+player2_wins = 0
+winning_score = 4
+
+while player1_wins < winning_score and player2_wins < winning_score:
+    print(f"player 1 : {player1_wins} and player 2 : {player2_wins}")
+    Player_1 = input("player_1 , Make your move : ").lower()
+    print(f"player_2 , Make your move : {computerMove}")
+    Player_2 = computerMove
