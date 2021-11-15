aee7105663dae31ca3631939def5467c4dc54753
MaryamJokar <maryam.79jokar@gmail.com>
2021-11-15 17:39:28 +0330

    def add task

--- to_do_list.py
+++ to_do_list.py
@@ -4,4 +4,13 @@ import tkinter.messagebox
 root = tkinter.Tk()
 root.title("to do list")
 
+
+#create GUI
+Listbox_task=tkinter.Listbox(root,height=3, width=50)
+
+Listbox_task.pack()
+
+
+
+
 root.mainloop()
